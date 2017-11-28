b9a5ad4fa45cd7b3846070b1b9fe380ae9c964ae
yk <wcyk@fake-email.com>
2017-11-28 23:09:29 +0800

    Fixed bugs and Added support for new artifacts
    - Fix path of RecentFileCache.bcf
    - Fix filename of Microsoft-Windows-DriverFrameworks-UserMode%4Operational.evtx
    - Add support for recursive extraction of directories

--- artifacts.py
+++ artifacts.py
@@ -1,9 +1,9 @@
 LOC_REG = u'/Windows/System32/config/'
 LOC_WINEVT = u'/Windows/System32/winevt/logs/'
-LOC_APPCOMPAT = u'/Windows/AppCompat/Programs/'
+LOC_AMCACHE = u'/Windows/AppCompat/Programs/'
 LOC_RECENT = u'/AppData/Roaming/Microsoft/Windows/Recent/'
 
-SYSTEM_FILE = [
+SYSTEM_FILE = [  # [artifact_path, destination_directory]
     [LOC_REG + u'SAM', u'/Registry/'],
     [LOC_REG + u'SECURITY', u'/Registry/'],
     [LOC_REG + u'SOFTWARE', u'/Registry/'],
@@ -18,37 +18,38 @@ SYSTEM_FILE = [
     [LOC_WINEVT + u'Security.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Setup.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'System.evtx', u'/OSLogs/'],
-    [LOC_WINEVT + u'Microsoft-Windows-DriverFrameworks-UserMode-Operational.evtx', u'/OSLogs/'],
+    [LOC_WINEVT + u'Microsoft-Windows-DriverFrameworks-UserMode%4Operational.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Microsoft-Windows-PowerShell%4Operational.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Microsoft-Windows-TaskScheduler%4Operational.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx', u'/OSLogs/'],
     [LOC_WINEVT + u'Microsoft-Windows-Windows Firewall With Advanced Security%4Firewall.evtx', u'/OSLogs/'],
 
-    [LOC_APPCOMPAT + u'Amcache.hve', u'/MRU/Prog/'],
-    [LOC_APPCOMPAT + u'Programs/RecentFileCache.bcf', u'/MRU/Prog/'],
+    [LOC_AMCACHE + u'Amcache.hve', u'/MRU/Prog/'],
+    [LOC_AMCACHE + u'RecentFileCache.bcf', u'/MRU/Prog/'],
 
     [u'/Windows/Inf/setupapi.dev.log', u'/Registry/']
 ]
 
-SYSTEM_DIR = [
-    [u'/Windows/Prefetch', u'/MRU/Prog/prefetch/'],
-    [u'/Windows/System32/sru', u'/MRU/Prog/srum/'],
-    [u'/Windows/System32/wbem/Repository', u'/MRU/Prog/sccm/']
+SYSTEM_DIR = [  # [artifact_path, destination_directory, isRecursive]
+    # [u'/$Recycle.Bin', u'/Recycle/', True],
+    [u'/Windows/Prefetch', u'/MRU/Prog/prefetch/', False],
+    [u'/Windows/System32/sru', u'/MRU/Prog/srum/', False],
+    [u'/Windows/System32/wbem/Repository', u'/MRU/Prog/sccm/', False]
 ]
 
-USER_FILE = [
+USER_FILE = [  # [artifact_path, destination_directory]
     [u'/NTUSER.DAT', u'/Registry/'],
     [u'/AppData/Local/Microsoft/Windows/UsrClass.dat', u'/Registry/']
 ]
 
-USER_DIR = [
-    [LOC_RECENT, u'/MRU/Files/lnk/'],
-    [LOC_RECENT + u'AutomaticDestinations', u'/MRU/Files/jmp/'],
-    [LOC_RECENT + u'CustomDestinations', u'/MRU/Files/jmp/'],
-    [u'/AppData/Local/Microsoft/Windows/WebCache', u'/MRU/Files/webcache/']
+USER_DIR = [  # [artifact_path, destination_directory, isRecursive]
+    [LOC_RECENT, u'MRU/Files/lnk/', False],
+    [LOC_RECENT + u'AutomaticDestinations', u'/MRU/Files/jmp/', False],
+    [LOC_RECENT + u'CustomDestinations', u'/MRU/Files/jmp/', False],
+    [u'/AppData/Local/Microsoft/Windows/WebCache', u'/MRU/Files/webcache/', False]
 ]
 
-FILE_ADS = [
+FILE_ADS = [  # [artifact_path, destination_directory, ADS Name]
     # [u'/$Extend/$UsnJrnl', u'/filesystem/', u'$J']
 ]
